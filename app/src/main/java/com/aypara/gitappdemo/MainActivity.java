0d3f9205eb83ef7c1d0c83c21e50112c4db0f46e
saeed <leysi.saeed@gmail.com>
2019-05-05 16:13:34 +0430

    add link to bazaar store login

--- app/src/main/java/com/aypara/gitappdemo/MainActivity.java
+++ app/src/main/java/com/aypara/gitappdemo/MainActivity.java
@@ -1,13 +1,28 @@
 package com.aypara.gitappdemo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnBazar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnBazar=findViewById(R.id.btn_bazar);
+        btnBazar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("bazaar://login"));
+                intent.setPackage("com.farsitel.bazaar");
+                startActivity(intent);
+            }
+        });
+
     }
 }
